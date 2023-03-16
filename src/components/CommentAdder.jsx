86eaa4e532d90a2c37e38a42055af3754a6936ed
JoshFost <josh.foster@hotmail.co.uk>
2023-03-16 12:01:26 +0000

    hard coded the user

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -4,19 +4,21 @@ import { Link } from "react-router-dom";
 
 const CommentAdder = ({ review_id, setComments }) => {
   const [comment, setComment] = useState("");
-  const [author, setAuthor] = useState("");
+  // const [author, setAuthor] = useState("");
+  const user = "tickle122";
   const [posted, setPosted] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (comment === "" || author === "") {
+    if (comment === "") {
+      // || author === ""
       setPosted(<p>Please fill in all the fields</p>);
     } else {
       setPosted(<p>Posing Comment...</p>);
-      postCommentByReviewId(review_id, author, comment)
+      postCommentByReviewId(review_id, user, comment)
         .then((newComment) => {
           setComment("");
-          setAuthor("");
+          // setAuthor("");
           setPosted(<p>Comment posted successfully!</p>);
           setComments((previousComments) => [newComment, ...previousComments]);
         })
@@ -29,13 +31,13 @@ const CommentAdder = ({ review_id, setComments }) => {
   return (
     <form onSubmit={handleSubmit}>
       <h3>Write a comment...</h3>
-      <label htmlFor="name">Username: </label>
+      {/* <label htmlFor="name">Username: </label>
       <textarea
         id="name"
         placeholder="Please enter your username..."
         value={author}
         onChange={(event) => setAuthor(event.target.value)}
-      ></textarea>
+      ></textarea> */}
 
       <label htmlFor="comment">Comment: </label>
       <textarea
